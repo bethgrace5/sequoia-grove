71928f3bab8498598d277d3ca08dfc40108f4e94
Beth Armitage <bethgrace5@gmail.com>
2015-07-25 22:23:59 -0700

    yeoman is christmas, it's like 'here, have a shiny new angular webapplication template with one command'

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sequoiaGroveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sequoiaGroveApp
+ */
+angular.module('sequoiaGroveApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
