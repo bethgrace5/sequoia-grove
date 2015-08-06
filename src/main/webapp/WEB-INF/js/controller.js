154547946c461d85647a22b5c5d893258358de0b
Beth Armitage <bethgrace5@gmail.com>
2015-08-06 11:38:14 -0700

    basic front/backend without bower or npm dependencies

---
+++ src/main/webapp/WEB-INF/js/controller.js
@@ -0,0 +1,33 @@
+
+
+
+//the first asynchronous request will be the page #1
+var page=1;
+
+function carsCtrl($scope, $http){
+    //when the user enters in the site the 3 cars are loaded through SpringMVC
+    //by default AngularJS cars is empty
+    $scope.cars = [];
+
+    //that is the way for bindding 'click' event to a AngularJS function
+    //javascript cannot know the context, so we give it as a parameter
+    $scope.load = function(context){
+        //Asynchronous request, if you know jQuery, this one works like $.ajax
+        $http({
+            url: context+'/load/'+page,
+            method: "POST",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function (data, status, headers, config) {
+            //data contains the model which is send it by the Spring controller in JSON format
+            //$scope.cars.push is the way to add new cars into $scope.cars array
+            for(i=0; i< data.carList.length; i++)
+            $scope.cars.push(data.carList[i]);
+
+        page++; //updating the page
+        page%=5; //our bean contains 15 cars, 3 cars par page = 5 pages, so page 5=0
+
+        }).error(function (data, status, headers, config) {
+            alert(status);
+        });           
+    }
+}
