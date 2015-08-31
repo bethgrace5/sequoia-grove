5318841d51b50b95b2ba194e526008b6b3352e84
Beth Armitage <bethgrace5@gmail.com>
2015-08-30 20:37:18 -0700

    more request styling

--- src/main/webapp/WEB-INF/js/controllers/request.js
+++ src/main/webapp/WEB-INF/js/controllers/request.js
@@ -14,8 +14,8 @@ angular.module('sequoiaGroveApp')
       { employee: "John", startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending"},
       { employee: "Emma", startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending"},
       { employee: "Emma", startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending"},
-      { employee: "Andy", startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending"},
-      { employee: "Sawyer", startDate: "May 20", endDate: "May 20", totalDays: "1", status: "granted"},
+      { employee: "Andy", startDate: "May 25", endDate: "May 28", totalDays: "3", status: "approved"},
+      { employee: "Sawyer", startDate: "May 20", endDate: "May 20", totalDays: "1", status: "approved"},
       { employee: "Blue", startDate: "May 15", endDate: "May 15", totalDays: "1", status: "denied"}
     ];
 
