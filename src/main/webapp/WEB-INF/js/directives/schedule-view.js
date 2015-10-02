e648952450a5034606ad317b1490de62692888fb
Beth Armitage <bethgrace5@gmail.com>
2015-10-02 00:21:27 -0700

    share scope with parent for translation to work through directive

--- src/main/webapp/WEB-INF/js/directives/schedule-view.js
+++ src/main/webapp/WEB-INF/js/directives/schedule-view.js
@@ -3,6 +3,7 @@
 angular.module('sequoiaGroveApp').directive('scheduleView',[ '$translate', function($translate) {
     return {
         restrict: 'E',
+        scope: false,
         templateUrl: 'templates/schedule-view.html'
     };
 }]);
