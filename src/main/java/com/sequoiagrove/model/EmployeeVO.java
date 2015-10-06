e3817e535aa5d661195dc7e9f14c5103f36ff896
Beth Armitage <bethgrace5@gmail.com>
2015-10-06 09:46:52 -0700

    cleanup extra stuff

--- src/main/java/com/sequoiagrove/model/EmployeeVO.java
+++ src/main/java/com/sequoiagrove/model/EmployeeVO.java
@@ -1,28 +1,14 @@
 package com.sequoiagrove.model;
 
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
-    @XmlRootElement (name = "employee")
-@XmlAccessorType(XmlAccessType.NONE)
-    public class EmployeeVO implements Serializable
+public class EmployeeVO implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
-    @XmlAttribute
         private Integer id;
-
-    @XmlElement
         private String firstName;
-
-    @XmlElement
         private String lastName;
-
-    @XmlElement
         private String email;
 
     public EmployeeVO(Integer id, String firstName, String lastName, String email) {
@@ -37,14 +23,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 
     }
 
+
+@Override
+    public String toString() {
+        return "EmployeeVO [id=" + id + ", firstName=" + firstName
+            + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+
     //Setters and Getters
-
-    @Override
-        public String toString() {
-            return "EmployeeVO [id=" + id + ", firstName=" + firstName
-                + ", lastName=" + lastName + ", email=" + email + "]";
-        }
-
     public int getId() {
         return id;
     }
