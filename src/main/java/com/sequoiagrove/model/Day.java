827f3f1be91e2a3e2f52ea921ec451c8a40f8bc4
Beth Armitage <bethgrace5@gmail.com>
2015-10-06 23:13:21 -0700

    setup for creation of schedule

---
+++ src/main/java/com/sequoiagrove/model/Day.java
@@ -0,0 +1,72 @@
+
+package com.sequoiagrove.model;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Day {
+    Date date;
+    String weekday;
+    HashMap<Employee, Shift> day = new HashMap<Employee, Shift>();
+
+    public Day(){}
+    public Day(Date date, String weekday) {
+        this.date = date;
+        this.weekday = weekday;
+        setShifts();
+    }
+
+    public void setShifts() {
+        // gets all shifts and adds them to the day as empty
+        // slots that need to all be filled
+    }
+
+    public void scheduleEmployee(Employee employee, Shift shift) {
+        // check that this employee is able to work this shift
+        // and that they are not already scheduled for this day
+        // if the day is a weekday, add weekday hours if the day is a 
+        // saturday or sunday, make sure to add those hours insttead
+        day.put(employee, shift);
+    }
+
+    public void deleteScheduledEmployee(Employee employee) {
+        // finds the employee in the list, and deletes that 
+        // entry from the map
+    }
+
+    public boolean validateDay() {
+        // checks that all shifts are filled, only one employee is
+        // scheduled per shift, the employee is trained to work the 
+        // position that the shift requires, the employee is not 
+        // working in a time slot that overlaps with annother employee
+        // that they cannot work with, and that this day is not in the 
+        // range of any days they have been granted a request off
+
+        return true;
+    }
+
+    public void setHoliday(String type) {
+        // a day may be a full or half holiday, 
+        // depending upon the type, shifts will be marked
+        // as filled by holiday
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public HashMap<Employee, Shift> getDay() {
+        return day;
+    }
+
+    public void setDay(HashMap<Employee, Shift> day) {
+        this.day = day;
+    }
+}
