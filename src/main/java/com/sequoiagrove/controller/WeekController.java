dfa7c76a74fdbb5e15ffe73b428be1810447d992
Beth Armitage <bethgrace5@gmail.com>
2015-10-07 16:53:06 -0700

    begin setup for week schedule

---
+++ src/main/java/com/sequoiagrove/controller/WeekController.java
@@ -0,0 +1,29 @@
+package com.sequoiagrove.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
+
+import com.sequoiagrove.model.Week;
+//import com.sequoiagrove.dao.WeekDAO;
+
+
+@Controller
+public class WeekController {
+
+    //@RequestMapping(value = "/week/{date}")
+        //public ResponseEntity<Employee> getWeek(@PathVariable("date") Date startingMonday) {
+            //return new ResponseEntity<Employee>(EmployeeDAO.getWeek(startingMonday), HttpStatus.OK);
+
+            //return new ResponseEntity(HttpStatus.NOT_FOUND);
+        //}
+
+}
+
