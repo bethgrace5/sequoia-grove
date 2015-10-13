7d4dbc311192b3cc968ee1fc22a299f78d3a3b62
Beth Armitage <bethgrace5@gmail.com>
2015-10-13 01:18:16 -0700

    remove unnecessary file

--- src/main/java/com/sequoiagrove/controller/MainController.java
+++ src/main/java/com/sequoiagrove/controller/MainController.java
@@ -12,8 +12,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.sequoiagrove.dao.DatabaseDAO;
-
 @Controller
 public class MainController {
 
