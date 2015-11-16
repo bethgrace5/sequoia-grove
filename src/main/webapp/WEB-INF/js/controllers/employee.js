f11cd8f6a9c45ef3ea6c0c58bb5b40eb4d220b5f
Sunny Sumal <sunnysumal@gmail.com>
2015-11-15 18:19:06 -0800

    fixed time format in JSON object

--- src/main/webapp/WEB-INF/js/controllers/employee.js
+++ src/main/webapp/WEB-INF/js/controllers/employee.js
@@ -77,19 +77,19 @@ angular.module('sequoiaGroveApp')
         phoneNumber:"000-000-0000",
         clockNumber:"10",
         avail:
-        { mon: [ {startTime:"800", endTime:"2000"}
+        { mon: [ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          tue:[ {startTime:"800", endTime:"2000"}
+          tue:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          wed:[ {startTime:"800", endTime:"2000"}
+          wed:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          thu:[ {startTime:"800", endTime:"2000"}
+          thu:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          fri:[ {startTime:"800", endTime:"2000"}
+          fri:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          sat:[ {startTime:"800", endTime:"2000"}
+          sat:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ],
-          sun:[ {startTime:"800", endTime:"2000"}
+          sun:[ {startHour: 8, startMin: 0, endHour: 20, endMin: 0}
           ]
         }
       },
@@ -102,19 +102,19 @@ angular.module('sequoiaGroveApp')
         phoneNumber:"111-111-1111",
         clockNumber:"11",
         avail:
-        { mon:[ {startTime:"800", endTime:"1900"}
+        { mon:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          tue:[ {startTime:"800", endTime:"1900"}
+          tue:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          wed:[ {startTime:"800", endTime:"1900"}
+          wed:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          thu:[ {startTime:"800", endTime:"1900"}
+          thu:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          fri:[ {startTime:"800", endTime:"1900"}
+          fri:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          sat:[ {startTime:"800", endTime:"1900"}
+          sat:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ],
-          sun:[ {startTime:"800", endTime:"1900"}
+          sun:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 0}
           ]
         }
       },
@@ -127,19 +127,19 @@ angular.module('sequoiaGroveApp')
         phoneNumber:"222-222-2222",
         clockNumber:"12",
         avail:
-        { mon:[ {startTime:"800", endTime:"2100"}
+        { mon:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          tue:[ {startTime:"800", endTime:"2100"}
+          tue:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          wed:[ {startTime:"800", endTime:"2100"}
+          wed:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          thu:[ {startTime:"800", endTime:"2100"}
+          thu:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          fri:[ {startTime:"800", endTime:"2100"}
+          fri:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          sat:[ {startTime:"800", endTime:"2100"}
+          sat:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ],
-          sun:[ {startTime:"800", endTime:"2100"}
+          sun:[ {startHour: 8, startMin: 0, endHour: 21, endMin: 0}
           ]
         }
       },
@@ -152,19 +152,19 @@ angular.module('sequoiaGroveApp')
         phoneNumber:"333-333-3333",
         clockNumber:"13",
         avail:
-        { mon:[ {startTime:"900", endTime:"2000"}
+        { mon:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          tue:[ {startTime:"900", endTime:"2000"}
+          tue:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          wed:[ {startTime:"900", endTime:"2000"}
+          wed:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          thu:[ {startTime:"900", endTime:"2000"}
+          thu:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          fri:[ {startTime:"900", endTime:"2000"}
+          fri:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          sat:[ {startTime:"900", endTime:"2000"}
+          sat:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ],
-          sun:[ {startTime:"900", endTime:"2000"}
+          sun:[ {startHour: 9, startMin: 0, endHour: 20, endMin: 0}
           ]
         }
       },
@@ -177,187 +177,24 @@ angular.module('sequoiaGroveApp')
         phoneNumber:"444-444-4444",
         clockNumber:"14",
         avail:
-        { mon:[ {startTime:"800", endTime:"1930"}
+        { mon:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          tue:[ {startTime:"800", endTime:"1930"}
+          tue:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          wed:[ {startTime:"800", endTime:"1930"}
+          wed:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          thu:[ {startTime:"800", endTime:"1930"}
+          thu:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          fri:[ {startTime:"800", endTime:"1930"}
+          fri:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          sat:[ {startTime:"800", endTime:"1930"}
+          sat:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ],
-          sun:[ {startTime:"800", endTime:"1930"}
+          sun:[ {startHour: 8, startMin: 0, endHour: 19, endMin: 30}
           ]
         }
       }
     ];
-/*
-    $scope.employees=[
-      { id:"0", 
-        firstName:"Billy",
-        lastName:"Jean",
-        isManager:"0",
-        birthDate:"01/01/1995",
-        maxHoursPerWeek:"40",
-        phoneNumber:"000-000-0000",
-        clockNumber:"10",
-        avail:
-        { mon:[ startTime:"800",
-                endTime:"2000"
-          ],
-          tue:[ startTime:"800",
-                endTime:"2000"
-          ],
-          wed:[ startTime:"800",
-                endTime:"2000"
-          ],
-          thu:[ startTime:"800",
-                endTime:"2000"
-          ],
-          fri:[ startTime:"800",
-                endTime:"2000"
-          ],
-          sat:[ startTime:"800",
-                endTime:"2000"
-          ],
-          sun:[ startTime:"800",
-                endTime:"2000"
-          ]
-        }
-      },
-      { id:"1", 
-        firstName:"Is not",
-        lastName:"My Lover",
-        isManager:"0",
-        birthDate:"02/02/1996",
-        maxHoursPerWeek:"50",
-        phoneNumber:"111-111-1111",
-        clockNumber:"11",
-        avail:
-        { mon:[ startTime:"800",
-                endTime:"1900"
-          ],
-          tue:[ startTime:"800",
-                endTime:"1900"
-          ],
-          wed:[ startTime:"800",
-                endTime:"1900"
-          ],
-          thu:[ startTime:"800",
-                endTime:"1900"
-          ],
-          fri:[ startTime:"800",
-                endTime:"1900"
-          ],
-          sat:[ startTime:"800",
-                endTime:"1900"
-          ],
-          sun:[ startTime:"800",
-                endTime:"1900"
-          ]
-        }
-      },
-      { id:"2", 
-        firstName:"She's just",
-        lastName:"A girl",
-        isManager:"1",
-        birthDate:"03/03/1991",
-        maxHoursPerWeek:"50",
-        phoneNumber:"222-222-2222",
-        clockNumber:"12",
-        avail:
-        { mon:[ startTime:"800",
-                endTime:"2100"
-          ],
-          tue:[ startTime:"800",
-                endTime:"2100"
-          ],
-          wed:[ startTime:"800",
-                endTime:"2100"
-          ],
-          thu:[ startTime:"800",
-                endTime:"2100"
-          ],
-          fri:[ startTime:"800",
-                endTime:"2100"
-          ],
-          sat:[ startTime:"800",
-                endTime:"2100"
-          ],
-          sun:[ startTime:"800",
-                endTime:"2100"
-          ]
-        }
-      },
-      { id:"3", 
-        firstName:"Who claims",
-        lastName:"That I am",
-        isManager:"0",
-        birthDate:"04/04/1999",
-        maxHoursPerWeek:"40",
-        phoneNumber:"333-333-3333",
-        clockNumber:"13",
-        avail:
-        { mon:[ startTime:"900",
-                endTime:"2000"
-          ],
-          tue:[ startTime:"900",
-                endTime:"2000"
-          ],
-          wed:[ startTime:"900",
-                endTime:"2000"
-          ],
-          thu:[ startTime:"900",
-                endTime:"2000"
-          ],
-          fri:[ startTime:"900",
-                endTime:"2000"
-          ],
-          sat:[ startTime:"900",
-                endTime:"2000"
-          ],
-          sun:[ startTime:"900",
-                endTime:"2000"
-          ]
-        }
-      },
-      { id:"4", 
-        firstName:"The",
-        lastName:"One",
-        isManager:"1",
-        birthDate:"05/05/1997",
-        maxHoursPerWeek:"40",
-        phoneNumber:"444-444-4444",
-        clockNumber:"14",
-        avail:
-        { mon:[ startTime:"800",
-                endTime:"1930"
-          ],
-          tue:[ startTime:"800",
-                endTime:"1930"
-          ],
-          wed:[ startTime:"800",
-                endTime:"1930"
-          ],
-          thu:[ startTime:"800",
-                endTime:"1930"
-          ],
-          fri:[ startTime:"800",
-                endTime:"1930"
-          ],
-          sat:[ startTime:"800",
-                endTime:"1930"
-          ],
-          sun:[ startTime:"800",
-                endTime:"1930"
-          ]
-        }
-      }
-    ];
-*/
+
     $scope.current = 0;
 
     $scope.newAvail = {day:'', start:'', end:''};
