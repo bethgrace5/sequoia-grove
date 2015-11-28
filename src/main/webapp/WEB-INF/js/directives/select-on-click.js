1f3314ed35e639ae9d8f80cb5a27df7a773fd78a
Beth Armitage <bethgrace5@gmail.com>
2015-11-27 17:25:22 -0800

    widen edit schedule

--- src/main/webapp/WEB-INF/js/directives/select-on-click.js
+++ src/main/webapp/WEB-INF/js/directives/select-on-click.js
@@ -124,8 +124,9 @@ angular.module('sequoiaGroveApp').directive('selectOnClick', ['$window', '$timeo
             }
             // add shift to update list if necessary
             $scope.checkIfShiftExists(attrs.day, newId, attrs.sid)
-            this.click();
+            //this.click();
             //this.blur();
+            element.context.classList.add('schedule-edit-highlight');
           }
         }
 
