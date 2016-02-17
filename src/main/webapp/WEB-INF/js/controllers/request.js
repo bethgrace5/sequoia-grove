6c6a32078e192e645ad9b4c1d7408975b38cb927
bahamutr13 <tedlpascua@gmail.com>
2016-02-16 19:41:08 -0800

    #32 Commentation Improvements and Reordering of Functions

--- src/main/webapp/WEB-INF/js/controllers/request.js
+++ src/main/webapp/WEB-INF/js/controllers/request.js
@@ -6,19 +6,41 @@
  * # RequestCtrl
  * Controller for requesting vacation
  */
+//+---------------
+//| Directory
+//+--------------
+//| Minor_Function
+//|   - Date_Gatherer
+//|   - Ease_of_Access
+//| Employee_User_Setup
+//|   - SubmitGet_Request
+//|   - Dates_Collider
+//| Manager_View
+//|   - Retrieve_Requests
+//|   - Manager_Change_Submit_Request
+//| Toggles
+//| ?????
+//| Initialize_Testing_Extreme
+
 
 angular.module('sequoiaGroveApp')
 .controller('RequestCtrl', function ($scope, $log, $rootScope, $http, $location) {
 
+  /****************** Check and Balances ****************************/
   $rootScope.lastPath = '/schedule';
   $rootScope.lastPath = '/request';
-
   // user is not logged in
   if ($rootScope.loggedIn == false) {
     $location.path('/login');
   }
 
-  //Date Gatherer
+  /****************** Minor_Functions ****************************/
+  //-----------------
+  // Date_Gatherer
+  //-----------------
+  $scope.requestDateStart;// = $scope.minDateStart;
+  $scope.requestDateEnd;  //= $scope.minDateStart;
+
   $scope.today = new Date();
   $scope.minDateStart = new Date(
     $scope.today.getFullYear(),
@@ -26,17 +48,44 @@ angular.module('sequoiaGroveApp')
     $scope.today.getDate() + 14
     );
 
-  $scope.requestDateStart = $scope.minDateStart;
-  $scope.requestDateEnd   = $scope.minDateStart;
-
   $scope.updateEnd = function(){
     if(moment($scope.requestDateStart).isAfter($scope.requestDateEnd)){
       $scope.requestDateEnd = $scope.requestDateStart;
     }
   }
 
-  //Submit Request 
+  //-----------------
+  // Ease_of_Access
+  //-----------------
+  $scope.totalDays = function(a, b){
+    var dog = moment(a);
+    var cool = moment(b)
+    return cool.diff(dog, 'days');
+  }
+
+  //Change Date into a specific format
+  $scope.defaultDate = function(a){
+    return moment(a).format("MM-DD-YYYY");
+  }
+
+  /****************** Employee_User_Setup ****************************/
+  //------------------------------
+  //  SubmitGet_Request
+  //------------------------------
   $scope.userRequests;
+
+  $scope.getCurrentEmployeeRequest = function() {
+    $http({
+      url: '/sequoiagrove/request/get/current/employee/'+
+      $rootScope.loggedInUser.id,
+      method: "POST"
+    }).success(function(data, status) {
+      $scope.userRequests = data.request;
+      $log.debug("Current Requests");
+      $log.debug($scope.userRequests);
+    });
+  }
+
   $scope.submitRequest = function(){
     if($scope.checkDatesCollide()){
       return;
@@ -58,7 +107,13 @@ angular.module('sequoiaGroveApp')
       $log.error('Error submiting request ', status, data);
     });
   }
-  //Check If User Requests History Conflicts with User's Submit Request
+
+  //---------------
+  //Dates_Collider
+  //---------------
+  /*This will take the requestDateStart / requestDateEnd and compare it
+   *to userRequestes, it will return true if it conflicts with eachother */
+
   $scope.checkDatesCollide = function(){
     $log.debug($scope.userRequests);
 
@@ -95,10 +150,62 @@ angular.module('sequoiaGroveApp')
     return false;
   }
 
-  //Manager's Sumbit Request
-  $scope.seeEmployees = 1; //When Manager Wants to see all employees... Test?
-  $scope.seeTargetEmployee = 0;
-  $scope.selectedEmployee;   //Info On Specific
+  /****************** Manager_View ****************************/
+  //-----------------------------------
+  //Retrieve_Requests
+  //-----------------------------------
+  $scope.employees;
+  $scope.allRequests;
+  $scope.pendingRequests;
+
+  $scope.getEmployees = function() {
+    $http({
+      url: '/sequoiagrove/employees',
+      method: "GET"
+    }).success(function (data, status, headers, config) {
+      $log.debug("All Employeess");
+      $scope.employees = data.employees;
+      $log.debug(data);
+    }).error(function (data, status, headers, config) {
+      $log.error(status + " Error obtaining position data: " + data);
+    });
+  }
+
+  $scope.getAllRequests = function() {
+    $http({
+      url: '/sequoiagrove/request/get',
+      method: "GET"
+    }).success(function (data, status, headers, config) {
+      $log.debug("All Requests");
+      $scope.allRequests = data.requestStatus;
+      $log.debug($scope.allRequests);
+    }).error(function (data, status, headers, config) {
+      $log.error(status + " Error obtaining position data: " + data);
+    });
+  }
+
+  $scope.getPendingRequests = function() {
+    $http({
+      url: '/sequoiagrove/request/get/pending',
+      method: "GET"
+    }).success(function (data, status, headers, config) {
+      $log.debug("Pending Requests");
+      $log.debug(data);
+      $scope.pendingRequests = data.requestStatus;
+      $log.debug($scope.pendingRequests);
+    }).error(function (data, status, headers, config) {
+      $log.error(status + " Error obtaining position data: " + data);
+    });
+  }
+
+  //----------------------------------
+  //Manager_Change_Submit_Request
+  //----------------------------------
+  $scope.selectedEmployee;
+  $scope.targetEmployee = function(employee){
+    $scope.selectedEmployee = employee
+      $log.debug($scope.selectedEmployee);
+  }
 
   $scope.managerSubmitRequest = function(){
     var obj = { "eid": $scope.selectedEmployee.id,
@@ -119,82 +226,6 @@ angular.module('sequoiaGroveApp')
     });
   }
 
-  //Toggle Employees View
-  $scope.changeEmployeeView = function(){
-    if($scope.seeEmployees){
-      $scope.seeEmployees = 0;
-    }
-    else{
-      $scope.seeEmployees = 1;
-    }
-  }
-  //Toggle See Target Employee
-  $scope.targetEmployee = function(employee){
-    $scope.selectedEmployee = employee
-      $log.debug($scope.selectedEmployee);
-    $scope.seeTargetEmployee = 1;
-  }
-
-  //Get All Employees 
-  $scope.employees;
-  $scope.getEmployees = function() {
-    $http({
-      url: '/sequoiagrove/employees',
-      method: "GET"
-    }).success(function (data, status, headers, config) {
-      $log.debug("All Employeess");
-      $scope.employees = data.employees;
-      $log.debug(data);
-    }).error(function (data, status, headers, config) {
-      $log.error(status + " Error obtaining position data: " + data);
-    });
-  }
-
-  //Get All Request
-  $scope.allRequests;
-  $scope.getAllRequests = function() {
-    $http({
-      url: '/sequoiagrove/request/get',
-      method: "GET"
-    }).success(function (data, status, headers, config) {
-      $log.debug("All Requests");
-      $scope.allRequests = data.requestStatus;
-      $log.debug($scope.allRequests);
-    }).error(function (data, status, headers, config) {
-      $log.error(status + " Error obtaining position data: " + data);
-    });
-  }
-
-  //Get Request that are pending
-  $scope.pendingRequests;
-  $scope.getPendingRequests = function() {
-    $http({
-      url: '/sequoiagrove/request/get/pending',
-      method: "GET"
-    }).success(function (data, status, headers, config) {
-      $log.debug("Pending Requests");
-      $log.debug(data);
-      $scope.pendingRequests = data.requestStatus;
-      $log.debug($scope.pendingRequests);
-    }).error(function (data, status, headers, config) {
-      $log.error(status + " Error obtaining position data: " + data);
-    });
-  }
-
-  //Get Current User Requests
-  $scope.getCurrentEmployeeRequest = function() {
-    $http({
-      url: '/sequoiagrove/request/get/current/employee/'+
-      $rootScope.loggedInUser.id,
-      method: "POST"
-    }).success(function(data, status) {
-      $scope.userRequests = data.request;
-      $log.debug("Current Requests");
-      $log.debug($scope.userRequests);
-    });
-  }
-
-  //Change Request
   $scope.changeRequest = function($requestID, $approverID, $is_approve) {
     $log.debug("changeRequest activated");
     $http({
@@ -207,21 +238,24 @@ angular.module('sequoiaGroveApp')
       $scope.getPendingRequests();
     });
   }
-  $scope.test = function(){
-    $log.debug("changeRequest activated"); 
+
+  //-------------------------
+  //Toggles
+  //------------------------- 
+  $scope.seeEmployees = 1; //When Manager Wants to see all employees... Test?
+  $scope.seeTargetEmployee = 0;
+
+  $scope.changeEmployeeView = function(){
+    if($scope.seeEmployees){
+      $scope.seeEmployees = 0;
+    }
+    else{
+      $scope.seeEmployees = 1;
+    }
   }
 
-  //Total Days
-  $scope.totalDays = function(a, b){
-    var dog = moment(a);
-    var cool = moment(b)
-      return cool.diff(dog, 'days');
-  }
-  //Change Date I
-  $scope.defaultDate = function(a){
-    return moment(a).format("MM-DD-YYYY");
-  }
-
+  //?????
+  //********* Not Sure What this is Placement, but might be important info**************
   $scope.countDisplay = 0 ;
 
   $scope.donGraph= {
@@ -243,17 +277,7 @@ angular.module('sequoiaGroveApp')
       ]
   };
 
-  $scope.previousRequests = [
-  { employee: "John",   startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending" },
-  { employee: "Emma",   startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending" },
-  { employee: "Emma",   startDate: "May 25", endDate: "May 28", totalDays: "3", status: "pending" },
-  { employee: "Andy",   startDate: "May 25", endDate: "May 28", totalDays: "3", status: "approved"},
-  { employee: "Sawyer", startDate: "May 20", endDate: "May 20", totalDays: "1", status: "approved"},
-  { employee: "Blue",   startDate: "May 15", endDate: "May 15", totalDays: "1", status: "denied"  }
-  ];
-
-  //---------- Initialize Testing Extreme ----------------\\
-
+  //********** Initialize Testing Extreme ***************************\
   $scope.testManager = function(){
     if ($rootScope.loggedInUser.isManager) {
       $log.debug("you are a Manager");
@@ -262,6 +286,7 @@ angular.module('sequoiaGroveApp')
       $log.debug("you are a  Employee");
     }
   } 
+
   $scope.init = function(){
     $log.debug($scope.previousRequests);
     //$scope.changeRequest($rootScope.loggedInUser.id, $rootScope.loggedInUser.id , 1);
@@ -270,13 +295,7 @@ angular.module('sequoiaGroveApp')
     $scope.getPendingRequests();
     $scope.getEmployees();
     $scope.testManager();
-    //$scope.checkDates();
   }
+
   $scope.init();
-
-  //----Temporary Function Location -----
-
-
-
-
 });
