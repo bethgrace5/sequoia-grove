122002dd08f374df6f7f9924cd34403a6e9cdbe6
Beth Armitage <bethgrace5@gmail.com>
2016-03-06 12:44:19 -0800

    start working with manual control of transactions

--- src/main/java/com/sequoiagrove/controller/ScheduleController.java
+++ src/main/java/com/sequoiagrove/controller/ScheduleController.java
@@ -15,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
@@ -23,71 +25,89 @@ import com.sequoiagrove.model.ScheduleTemplate;
 import com.sequoiagrove.model.Day;
 import com.sequoiagrove.model.Scheduled;
 import com.sequoiagrove.controller.MainController;
+import org.springframework.transaction.TransactionStatus;
 
 
 @Controller
 public class ScheduleController {
 
     // Get current schedule template (current shifts) mm-dd-yyyy
-    @RequestMapping(value = "/schedule/template/{mon}")
-        public String getScheduleTemplate(Model model, @PathVariable("mon") String mon) {
+  @RequestMapping(value = "/schedule/template/{mon}")
+    public String getScheduleTemplate(Model model, @PathVariable("mon") final String mon) {
+      Integer count = 0;
 
-            JdbcTemplate jdbcTemplate = MainController.getJdbcTemplate();
+      final JdbcTemplate jdbcTemplate = MainController.getJdbcTemplate();
+      TransactionTemplate transactionTemplate = MainController.getTransactionTemplate();
 
-            List<ScheduleTemplate> schTempList = jdbcTemplate.query(
-                "select get_schedule('schedulecursor', '" + mon + "')",
-                    new RowMapper<ScheduleTemplate>() {
-                    public ScheduleTemplate mapRow(ResultSet rs, int rowNum) throws SQLException {
-                      System.out.println(rs.getType());
-                      System.out.println(rs.next());
-                      System.out.println(rs.getRow());
-                      //System.out.println(rs.getRowId());
-                      System.out.println(rs.getStatement());
-                      System.out.println(rs.getWarnings());
-                      System.out.println(rs.wasNull());
-                      System.out.println(rs.getMetaData().getColumnCount());
-
-                      for(int i=1; i<= rs.getMetaData().getColumnCount(); i++) {
-                        System.out.println("column name: ");
-                        System.out.println(rs.getMetaData().getColumnName(i));
-                        System.out.println("column type: ");
-                        System.out.println(rs.getMetaData().getColumnType(i));
-                        System.out.println("table name: ");
-                        System.out.println(rs.getMetaData().getTableName(i));
-                      }
-
-                      //System.out.println(rs.getObject(1).toString());
+      transactionTemplate.execute(new TransactionCallback<Object>() {
+        @Override
+        public Object doInTransaction(TransactionStatus status) {
 
 
-                    ScheduleTemplate schTmp = new ScheduleTemplate(
-                          rs.getInt("sid"),
-                          rs.getInt("pid"),
-                          rs.getString("location"),
-                          rs.getString("tname"),
-                          rs.getString("position"),
-                          rs.getString("wd_st"),// weekday start
-                          rs.getString("wd_ed"),// weekday end
-                          rs.getString("we_st"),// weekend start
-                          rs.getString("we_ed"),// weekend end
-                          new Day("mon", rs.getString("mon"), rs.getInt("mon_eid")),
-                          new Day("tue", rs.getString("tue"), rs.getInt("tue_eid")),
-                          new Day("wed", rs.getString("wed"), rs.getInt("wed_eid")),
-                          new Day("thu", rs.getString("thu"), rs.getInt("thu_eid")),
-                          new Day("fri", rs.getString("fri"), rs.getInt("fri_eid")),
-                          new Day("sat", rs.getString("sat"), rs.getInt("sat_eid")),
-                          new Day("sun", rs.getString("sun"), rs.getInt("sun_eid")) );
+      Integer count = jdbcTemplate.queryForObject(
+          "SELECT count(*) FROM published_schedule WHERE start_date = to_date(?,'dd-mm-yyyy')",Integer.class, mon);
+      return null;
+
+        }
+      });
+
+      model.addAttribute("ispublished", (count!=null && count > 0));
+      //model.addAttribute("template", schTempList);
+      return "jsonTemplate";
+}
+
+          /*
+          List<ScheduleTemplate> schTempList = jdbcTemplate.query(
+            "begin;" +
+            "select * from get_schedule('schedulecursor', '10-12-2015');" +
+            "fetch all in \"schedulecursor\"",
+            new RowMapper<ScheduleTemplate>() {
+              public ScheduleTemplate mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+
+                System.out.println(rs.getType());
+                System.out.println(rs.next());
+                System.out.println(rs.getRow());
+                //System.out.println(rs.getRowId());
+                System.out.println(rs.getStatement());
+                System.out.println(rs.getWarnings());
+                System.out.println(rs.wasNull());
+                System.out.println(rs.getMetaData().getColumnCount());
+
+                for(int i=1; i<= rs.getMetaData().getColumnCount(); i++) {
+                  System.out.println("column name: ");
+                  System.out.println(rs.getMetaData().getColumnName(i));
+                  System.out.println("column type: ");
+                  System.out.println(rs.getMetaData().getColumnType(i));
+                  System.out.println("table name: ");
+                  System.out.println(rs.getMetaData().getTableName(i));
+                }
+
+                //System.out.println(rs.getObject(1).toString());
+
+
+                ScheduleTemplate schTmp = new ScheduleTemplate(
+                    rs.getInt("sid"),
+                    rs.getInt("pid"),
+                    rs.getString("location"),
+                    rs.getString("tname"),
+                    rs.getString("position"),
+                    rs.getString("wd_st"),// weekday start
+                    rs.getString("wd_ed"),// weekday end
+                    rs.getString("we_st"),// weekend start
+                    rs.getString("we_ed"),// weekend end
+                    new Day("mon", rs.getString("mon"), rs.getInt("mon_eid")),
+                    new Day("tue", rs.getString("tue"), rs.getInt("tue_eid")),
+                    new Day("wed", rs.getString("wed"), rs.getInt("wed_eid")),
+                    new Day("thu", rs.getString("thu"), rs.getInt("thu_eid")),
+                    new Day("fri", rs.getString("fri"), rs.getInt("fri_eid")),
+                    new Day("sat", rs.getString("sat"), rs.getInt("sat_eid")),
+                    new Day("sun", rs.getString("sun"), rs.getInt("sun_eid")) );
 
                 return schTmp;
               }
-          });
-
-        Integer count = jdbcTemplate.queryForObject(
-                "SELECT count(*) FROM published_schedule WHERE start_date = to_date(?,'dd-mm-yyyy')",Integer.class, mon);
-
-        model.addAttribute("ispublished", (count!=null && count > 0));
-        model.addAttribute("template", schTempList);
-        return "jsonTemplate";
-    }
+            });
+            */
 
     // Update current schedule template (current shifts) dd/mm/yyyy
     @RequestMapping(value = "/schedule/update")
