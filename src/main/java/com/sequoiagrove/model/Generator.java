7421ee09bb05f8807cf6ed6b89170099808ecd62
bahamutr13 <tedlpascua@gmail.com>
2016-05-07 09:41:51 -0700

    #68 Seperated Table Gathering into the Generator class instead

--- src/main/java/com/sequoiagrove/model/Generator.java
+++ src/main/java/com/sequoiagrove/model/Generator.java
@@ -1,12 +1,21 @@
 package com.sequoiagrove.model;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sequoiagrove.model.Employee;
 import com.sequoiagrove.model.Request;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import com.sequoiagrove.controller.MainController;
+
 import java.util.*;
+
+//TODO: Need to Swap Functions arounds and organize them by sections
 
 public class Generator{
   HashMap
@@ -15,7 +24,7 @@ public class Generator{
 
   String startDate;
   String endDate;
-  Employee employees[];
+  List<DayShiftEmployee> dayShiftEmployeeList;
   Request requests[];
   //Or
   //String date[7]
@@ -58,6 +67,123 @@ public class Generator{
     generator.get(day).get(shift).put(employee, amount);
   }
 
+  public void getPastInformation(String startDate, String endDate){
+    JdbcTemplate jdbcTemplate = MainController.getJdbcTemplate();
+    dayShiftEmployeeList = jdbcTemplate.query(
+        " select day, shift_id, employee_id, count(*) AS worked" +
+        " from employee_shift_view " +
+        " where on_date >= '2016-03-21' AND on_date <= '2016-04-15' " +
+        " group by day, shift_id, employee_id " +
+        " order by day, shift_id, employee_id ",
+          new RowMapper<DayShiftEmployee>() {
+            public DayShiftEmployee mapRow(ResultSet rs, int rowNum) throws SQLException {
+              DayShiftEmployee es = new DayShiftEmployee(
+                rs.getInt("day"),
+                rs.getInt("shift_id"),
+                rs.getInt("employee_id"),
+                rs.getInt("worked")
+              );
+              return es;
+            }
+      });
+      for (int i = 0; i < dayShiftEmployeeList.size(); i++) {
+        add(convertDay(dayShiftEmployeeList.get(i).getDay()),
+                      dayShiftEmployeeList.get(i).getShift(),
+                      dayShiftEmployeeList.get(i).getEmployee(),
+                      dayShiftEmployeeList.get(i).getWorked() );
+      }
+      printFormation();
+  }
+
+  public void getEmployeeInformation(String startDate, String endDate){
+        JdbcTemplate jdbcTemplate = MainController.getJdbcTemplate();
+        String queryStr = "select * from employee_info_view";
+        List<Employee> empList = jdbcTemplate.query( queryStr,
+            new RowMapper<Employee>() {
+                public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    Employee employee = new Employee (
+                      rs.getInt("id"),
+                      rs.getInt("max_hrs_week"),
+                      rs.getInt("min_hrs_week"),
+                      rs.getBoolean("is_manager"),
+                      rs.getInt("clock_number"),
+                      rs.getString("first_name"),
+                      rs.getString("last_name"),
+                      rs.getString("phone_number"),
+                      rs.getString("email"),
+                      rs.getDate("birth_date"),
+                      parseHistory(rs.getString("history")),
+                      parsePositions(rs.getString("positions")),
+                      parseAvailability(rs.getString("avail")),
+                      false);
+
+                      // if the history end is an empty string, employee is current
+                      employee.setIsCurrent(
+                        employee.getHistory()
+                          .get(employee.getHistory().size() - 1)
+                          .getEnd() == ""
+                      );
+                    return employee;
+                }
+            });
+  }
+
+  public WeeklyAvail parseAvailability(String avail) {
+
+      WeeklyAvail entireAvail = new WeeklyAvail();
+
+      // split string into array with one string per day
+      String[] weekdays = avail.split("\\s+");
+
+      // for each day, add it to the weekly availability
+      for (String d : weekdays) {
+        String[] day = d.split(",");
+
+        for(int i=1; i<day.length; i++) {
+          String[] times = day[i].split(":");
+          entireAvail.add(day[0], times[0], times[1]);
+        }
+      }
+      return entireAvail;
+    }
+
+    // change History string to list of java objects
+    public List<Duration> parseHistory(String hist) {
+      List<Duration> historyList = new ArrayList<Duration>();
+
+      String[] histories = hist.split(",");
+      for (String h : histories) {
+        String[] times = h.split(":");
+        if(times.length == 2) {
+          historyList.add(new Duration(times[0], times[1]));
+        }
+        else {
+          historyList.add(new Duration(times[0]));
+        }
+      }
+      return historyList;
+    }
+
+    // change Position string to list of java objects
+    public List<String> parsePositions(String pos) {
+      if (pos == null) {
+        return new ArrayList<String>();
+      }
+      return new ArrayList<String>(Arrays.asList(pos.split(",")));
+    }
+
+
+  public String convertDay(Integer value){
+    if(value == 1) return "mon";
+    if(value == 2) return "tue";
+    if(value == 3) return "wen";
+    if(value == 4) return "thu";
+    if(value == 5) return "fri";
+    if(value == 6) return "sat";
+    if(value == 7) return "sun";
+    return "-------";
+  }
+
   public void add(String  day,
                           Integer shift,
                           Integer employee,
@@ -66,8 +192,7 @@ public class Generator{
     addShift(day, shift);
     addEmployee(day, shift, employee,  amount);
 
-  } 
-
+  }
 
   public void printAllDays(){
     for (String key : generator.keySet()) {
@@ -102,6 +227,7 @@ public class Generator{
      }
    }
  }
+
 
  public void trimByListRestriction(){
  }
