686eee3703ad7074d89f17750c4f62b5444944c3
Bethany Armitage <bethgrace5@gmail.com>
2016-08-13 13:37:00 -0700

    update screenshots, footer, and some of the text

--- src/main/resources/liquibase/changelogs/mock-data/cannot-work-with.sql
+++
@@ -1,9 +0,0 @@
-/* Employees who cannot be scheduled to work together */
-
-insert into cannot_work_with values( 1, 2);
-insert into cannot_work_with values( 1, 3);
-insert into cannot_work_with values( 1, 4);
-insert into cannot_work_with values( 10, 11);
-insert into cannot_work_with values( 10, 12);
-insert into cannot_work_with values( 15, 16);
-insert into cannot_work_with values( 15, 17);
